8d55e06423f54128f6d97f239bb429acbd40f6a1
Joseph Etim Ibok <josephibok36@gmail.com>
2024-08-08 12:20:36 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const TelegramBot = require("node-telegram-bot-api");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+const bot = new TelegramBot(process.env.TELEGRAM_API_TOKEN);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+bot.setWebHook(`${process.env.WEBHOOK_URL}/bot/${process.env.TELEGRAM_API_TOKEN}`);
+
+app.post(`/bot${process.env.TELEGRAM_API_TOKEN}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
+
+
