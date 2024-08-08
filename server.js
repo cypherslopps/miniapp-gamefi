6c72f27b8ba59b82bcc72ae93594e8840dc94322
Joseph Etim Ibok <josephibok36@gmail.com>
2024-08-08 13:07:32 +0100

    added bot menu button

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const bot = new TelegramBot(process.env.TELEGRAM_API_TOKEN);
 
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -24,6 +25,23 @@ bot.onText(/\/start/, (msg) => {
     console.log(msg);
     bot.sendMessage(msg.chat.id, process.env.GAME_URL);
 });
+
+const menuButton = {
+    text: "Open Game APP",
+    web_app: {
+        url: process.env.GAME_URL
+    }
+};
+
+bot.setChatMenuButton(menuButton)
+    .then(() => {
+        console.log('Menu button set successfully');
+    })
+    .catch(err => {
+        console.log("Error setting menu button", err);
+    })
+
+bot.setBotMenuButton()
 
 app.listen(PORT, () => {
     console.log(`Mini App listening on port http://localhost:${PORT}`);
