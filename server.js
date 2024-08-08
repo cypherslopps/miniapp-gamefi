eb39fb3a98eebca884145d51ca4aa7342fbfcc97
Joseph Etim Ibok <josephibok36@gmail.com>
2024-08-08 13:47:40 +0100

    testing /start command

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const bot = new TelegramBot(process.env.TELEGRAM_API_TOKEN);
 
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -23,7 +22,10 @@ app.post(`/bot${process.env.TELEGRAM_API_TOKEN}`, (req, res) => {
 
 bot.onText("/start", (msg) => {
     console.log(msg, "start command");
-    bot.sendMessage(msg.chat.id, process.env.GAME_URL);
+    bot.sendMessage(msg.chat.id, `
+        Dive into the multiverse \n\n
+        Kill all the alien enemies disrupting time jump.
+    `);
 });
 
 const menuButton = {
